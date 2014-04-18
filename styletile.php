d071b8a866582509a8e5096aea35b70e128179e8
rileywho <rileywho2@gmail.com>
2014-04-18 11:22:47 -0700

    Created a page and custom header for styletile

---
+++ styletile.php
@@ -0,0 +1,28 @@
+
+<?php
+/*
+Template Name: Styletile
+*/
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * Please see /external/starkers-utilities.php for info on Starkers_Utilities::get_template_parts()
+ *
+ * @package 	WordPress
+ * @subpackage 	FrankenStarkers
+ * @since 		FrankenStarkers 1.0
+ */
+Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header' ) ); ?>
+<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/styletile-header' ) ); ?>
+
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+<h2><?php the_title(); ?></h2>
+<?php the_content(); ?>
+<?php endwhile; ?>
+
+<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
